54d6acb826f738576ecda170cfc59ac0976b95f3
Zeyad <zeyadw526@gmail.com>
2025-07-20 20:19:32 +0300

    add activity detection

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,9 +20,31 @@ io.on('connection', (socket) => {
   // When a client connects, log their unique socket.id
   console.log(`User ${socket.id} connected`);
 
+  // Upon connection - only to user
+  socket.emit('message', 'Welcome to chat app!');
+
+  // Upon connection - everyone except the current user
+  socket.broadcast.emit(
+    'message',
+    `User ${socket.id.substring(0, 5)} connected`
+  );
+
   // Listen for 'message' events from the connected client
   socket.on('message', (data) => {
     console.log(data);
     io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
+  });
+
+  // When user disconnects - to all others
+  socket.on('disconnect', () => {
+    socket.broadcast.emit(
+      'message',
+      `User ${socket.id.substring(0, 5)} disconnected`
+    );
+  });
+
+  // Listen for activity
+  socket.on('activity', (name) => {
+    socket.broadcast.emit('activity', name);
   });
 });
